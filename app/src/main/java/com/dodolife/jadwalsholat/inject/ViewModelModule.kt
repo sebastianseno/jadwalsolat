313ff728b1b96375ea4be9011fa92a2dc1ed9efc
sebastianseno <seno@live.com>
2019-07-12 23:28:01 +0700

    View Model Module

---
+++ app/src/main/java/com/dodolife/jadwalsholat/inject/ViewModelModule.kt
@@ -0,0 +1,12 @@
+package com.dodolife.jadwalsholat.inject
+
+import androidx.lifecycle.ViewModelProvider
+import dagger.Binds
+import dagger.Module
+
+@Module
+abstract class ViewModelModule {
+
+    @Binds
+    abstract fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
+}
